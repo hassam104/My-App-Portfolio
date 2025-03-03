58f9bdf764b0f0457e7e919838965000f8a59af7
hassam104 <hassam625342@gmail.com>
2025-03-03 13:20:16 +0500

    wearher img link update

--- src/App.tsx
+++ src/App.tsx
@@ -78,7 +78,7 @@ function App() {
       id: 2,
       title: 'Weather checker App',
       description: 'A productivity application built with React Native for cross-platform mobile use. Includes task weather check on location based.',
-      image: 'assest/Image/Weahter.jpg',
+      image: 'https://drive.google.com/file/d/1BW5Z1hCTIm38NC533i8cMS5n8J5jX6Tx/view?usp=sharing',
       tags: ['React Native', 'Firebase', 'Redux', 'TypeScript'],
       link: 'https://github.com/hassam104/Weather-Checker',
       demoLink: 'https://www.linkedin.com/posts/hassam-zafar-075728264_react-nodejs-reactjs-activity-7272313022579171329-QLea?utm_source=share&utm_medium=member_desktop&rcm=ACoAAEDkLFkBO3v_2zRMwrQ6RuXU8VC8wfLAqB8'
